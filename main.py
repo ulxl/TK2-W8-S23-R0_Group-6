70dd20e71e0bc273e37dc5230fa7ec4fef4c2814
ulxl <laluhilmynet@gmail.com>
2025-10-20 07:04:28 +0700

    fix param

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Gambar yang diunggah.', use_column_width=True)
+    st.image(image, caption='Gambar yang diunggah.', use_container_width=True)
 
     # ---------- PREDIKSI + TOP-3 ----------
     with st.spinner("Memprediksi..."):
